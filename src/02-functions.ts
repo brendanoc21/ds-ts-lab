ef79ce1bdfc1ea65e984f02a6b4dc97d260ebc55
Brendan O'Connor <brendanoconnor2030@gmail.com>
2025-02-07 13:44:21 +0000

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -64,3 +64,13 @@ function highestExtension(cs: Colleague[]) { // Inferred retun type
   
   console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
   console.log(findFriends(friends, (friend) => friend.age < 35));
+
+  function addInterest(friend: Friend, interest: string): string[] {
+    if(!friend.interests){
+        friend.interests = []
+    }
+    friend.interests.push(interest)
+    return friend.interests
+  }
+
+  console.log(addInterest(friends[1], 'Politics'))
